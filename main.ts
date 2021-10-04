a93a4a48d900cd8f080fc3859e11b47c377a88f8
damiendonnelly <damiendonnelly@gmail.com>
2021-10-04 18:37:55 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,17 +2,78 @@ function explode () {
     item = 0
     tm.showNumber(item)
     music.playMelody("C5 B A G F E D C ", 120)
+    basic.showIcon(IconNames.Skull)
 }
 function wrong () {
     timerPause = timerPause - 100
+    basic.showString("No!")
 }
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    success = 1111
-    tm.showNumber(success)
-    music.playMelody("C D E F G A B C5 ", 120)
+    win()
 })
 input.onButtonPressed(Button.A, function () {
     tm.on()
+})
+function win () {
+    success = 1111
+    tm.showNumber(success)
+    music.playMelody("C D E F G A B C5 ", 120)
+    for (let index = 0; index < 30; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . # . # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . # . #
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . . . #
+            . . # . .
+            # . . . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . # . #
+            # . . . #
+            # # # # #
+            `)
+    }
+}
+input.onSound(DetectedSound.Loud, function () {
+    wrong()
+    basic.showString("Be quiet, shhhhh!")
+    basic.showString("" + (input.soundLevel()))
 })
 input.onButtonPressed(Button.AB, function () {
     item = 60
@@ -26,7 +87,11 @@ input.onButtonPressed(Button.B, function () {
     tm.off()
 })
 input.onGesture(Gesture.Shake, function () {
-    explode()
+    wrong()
+    basic.showString("Don't shake me!\"")
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+	
 })
 let tm: TM1637.TM1637LEDs = null
 let item = 0
@@ -46,6 +111,13 @@ DigitalPin.P2,
 basic.forever(function () {
     while (item != 0 && success == 0) {
         tm.showNumber(item)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
         if (item > 10) {
             music.playTone(196, music.beat(BeatFraction.Eighth))
         } else {
@@ -56,5 +128,12 @@ basic.forever(function () {
             explode()
         }
         basic.pause(timerPause)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
     }
 })
